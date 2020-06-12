ed57e7b1f80e06800b534db6f26257f1974d35b8
U-LuoDong <15599808806@qq.com>
2020-06-12 11:46:32 +0800

    测试redis连接

---
+++ bootsystem-admin/src/test/java/com/nsu/bootsystem/admin/Springboot01CacheApplicationTests.java
@@ -0,0 +1,34 @@
+package com.nsu.bootsystem.admin;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot01CacheApplicationTests {
+
+	@Autowired
+    StringRedisTemplate stringRedisTemplate;  //操作k-v都是字符串的
+	/**
+	 * Redis常见的五大数据类型
+	 *  String（字符串）、List（列表）、Set（集合）、Hash（散列）、ZSet（有序集合）
+	 *  stringRedisTemplate.opsForValue()[String（字符串）]
+	 *  stringRedisTemplate.opsForList()[List（列表）]
+	 *  stringRedisTemplate.opsForSet()[Set（集合）]
+	 *  stringRedisTemplate.opsForHash()[Hash（散列）]
+	 *  stringRedisTemplate.opsForZSet()[ZSet（有序集合）]
+	 */
+//	@Test
+//	public void test01(){
+//		//给redis中保存数据
+//	    stringRedisTemplate.opsForValue().append("msg","hello");
+//		String msg = stringRedisTemplate.opsForValue().get("msg");
+//		System.out.println(msg);
+//
+////		stringRedisTemplate.opsForList().leftPush("mylist","1");
+////		stringRedisTemplate.opsForList().leftPush("mylist","2");
+//	}
+}
